89c1e109b6ed626b7fe1bbfa4838a3609dc0b7f9
happymab <happymab.dev@pm.me>
2026-07-25 12:00:20 +0700

    chore: move settings and services to module

--- modules/hosts/nixos-vm/configuration.nix
+++ modules/hosts/nixos-vm/configuration.nix
@@ -5,79 +5,10 @@
   imports =
     [ # Include the results of the hardware scan.
       self.nixosModules.nixos-vmHardware
-      self.nixosModules.niri
       self.nixosModules.nixos-vmModule
+      self.nixosModules.niri
       self.nixosModules.myHomeManager
     ];
-
-  # Enable the Flakes feature and the accompanying new nix command-line tool
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Use systemd bootloader
-  boot.loader.systemd-boot.enable = true;
-
-  # Define the hostname
-  networking.hostName = "nixos-vm";
-
-  # Use latest kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  # Configure network connections interactively with nmcli or nmtui.
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Asia/Bangkok";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = false;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-
-  # Enable Spice VD agent (for VM guest)
-  services.spice-vdagentd.enable = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
