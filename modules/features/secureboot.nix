923405a2695fef92e002ee87e3cdd7bb0c7bd6df
happymab.dev <happymab.dev@pm.me>
2026-07-26 22:13:49 +0700

    feat: enable automatic key generation and enrollment, enable measured boot

--- modules/features/secureboot.nix
+++ modules/features/secureboot.nix
@@ -11,8 +11,29 @@
 
     boot.lanzaboote = {
       enable = true;
+      
       pkiBundle = "/var/lib/sbctl";
+      
+      autoGenerateKeys.enable = true;
+
+      autoEnrollKeys = {
+        enable = true;
+        autoReboot = true;
+      };
+
+      # Enable measured boot
+      measuredBoot = {
+        enable = true;
+        pcrs = [
+          0
+          4
+          7
+        ];
+      };
     };
+
+    # Required for measured boot
+    boot.initrd.systemd.enable = true;
 
     # Install sbctl key management and troubleshooting Secure Boot
     environment.systemPackages = [
