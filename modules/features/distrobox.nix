afcafe7cfdb6f501b04232a6ff31ecd8d26fa0b1
happymab.dev <happymab.dev@pm.me>
2026-08-01 21:28:24 +0700

    fix: reference self

--- modules/features/distrobox.nix
+++ modules/features/distrobox.nix
@@ -2,7 +2,7 @@
   flake.nixosModules.distrobox = { pkgs, ... }: {
 
     imports = [
-      flake.nixosModules.podman # Podman or Docker are required for distrobox
+      self.nixosModules.podman # Podman or Docker are required for distrobox
     ];
 
     environment.systemPackages = [ pkgs.distrobox ];
