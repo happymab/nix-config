265b203596f38f535e858d0021abade329140fa9
happymab.dev <happymab.dev@pm.me>
2026-08-03 17:09:33 +0700

    feat: use className for window separation on taskbar

--- modules/home/brave-config-mab.nix
+++ modules/home/brave-config-mab.nix
@@ -17,10 +17,10 @@
 
       xdg.desktopEntries =
         let
-          mkProfileEntry = name: profileDir: iconName: {
+          mkProfileEntry = name: profileDir: iconName: className: {
             "${name}" = {
               inherit name;
-              exec = "brave --profile-directory=\"${profileDir}\" %U";
+              exec = "brave --profile-directory=\"${profileDir}\" --class=\"${className}\" %U";
               icon = iconName;
               categories = [
                 "Network"
@@ -28,17 +28,17 @@
               ];
               terminal = false;
               comment = "Brave - ${name} Profile";
-              settings.StartupWMClass = "Brave-browser";
+              settings.StartupWMClass = className;
             };
           };
         in
-        mkProfileEntry "Default" "Standard" "brave_blue"
-        // mkProfileEntry "Development" "Development" "brave_pink"
-        // mkProfileEntry "Investing" "Investing" "brave_green"
-        // mkProfileEntry "Banking" "Banking" "brave_red"
-        // mkProfileEntry "VentureWise" "Venture Wise" "brave_yellow"
-        // mkProfileEntry "Crypto" "Crypto" "brave_orange"
-        // mkProfileEntry "Proton" "Proton" "brave_purple";
+        mkProfileEntry "Standard" "Default" "brave_blue" "brave-standard"
+        // mkProfileEntry "Development" "Development" "brave_pink" "brave-development"
+        // mkProfileEntry "Investing" "Investing" "brave_green" "brave-investing"
+        // mkProfileEntry "Banking" "Banking" "brave_red" "brave-banking"
+        // mkProfileEntry "Venture Wise" "VentureWise" "brave_yellow" "brave-venture-wise"
+        // mkProfileEntry "Crypto" "Crypto" "brave_orange" "brave-crypto"
+        // mkProfileEntry "Proton" "Proton" "brave_purple" "brave-proton";
     };
   };
 }
