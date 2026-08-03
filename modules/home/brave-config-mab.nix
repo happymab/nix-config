5b12bf91cc09b62ec099bf5d946166d92c8da298
happymab.dev <happymab.dev@pm.me>
2026-08-03 16:03:50 +0700

    fix: define icon with path

--- modules/home/brave-config-mab.nix
+++ modules/home/brave-config-mab.nix
@@ -4,20 +4,26 @@
     config = {
       home.packages = [ pkgs.brave ];
 
-      # Install custom icons to the icon theme directory
+      # Install icons into the hicolor icon theme hierarchy
       home.file = {
-        ".local/share/icons/brave-default.png".source = ./assets/icons/brave.png;
-        ".local/share/icons/brave-personal.png".source = ./assets/icons/brave_blue.png;
-        ".local/share/icons/brave-work.png".source = ./assets/icons/brave_pink.png;
+        ".local/share/icons/hicolor/48x48/apps/brave-default.png".source = ./assets/icons/brave.png;
+        ".local/share/icons/hicolor/48x48/apps/brave-personal.png".source = ./assets/icons/brave_blue.png;
+        ".local/share/icons/hicolor/48x48/apps/brave-work.png".source = ./assets/icons/brave_pink.png;
+
+        # Also install larger sizes for sharper rendering at high DPI
+        ".local/share/icons/hicolor/256x256/apps/brave-default.png".source = ./assets/icons/brave.png;
+        ".local/share/icons/hicolor/256x256/apps/brave-personal.png".source = ./assets/icons/brave_blue.png;
+        ".local/share/icons/hicolor/256x256/apps/brave-work.png".source = ./assets/icons/brave_pink.png;
       };
 
       xdg.desktopEntries =
         let
+          # Use iconName (lowercase) for the icon lookup, not name (capitalized)
           mkProfileEntry = name: profileDir: iconName: {
             "${name}" = {
               inherit name;
               exec = "brave --profile-directory=\"${profileDir}\" %U";
-              icon = "/home/mab/.local/share/icons/brave-${name}.png"; # Matches the filename without extension
+              icon = iconName; # Just the name, no path, no extension
               categories = [
                 "Network"
                 "WebBrowser"
@@ -28,9 +34,9 @@
             };
           };
         in
-        mkProfileEntry "Default" "Default" "default"
-        // mkProfileEntry "Personal" "Personal" "personal"
-        // mkProfileEntry "Work" "Work" "work";
+        mkProfileEntry "Default" "Default" "brave-default"
+        // mkProfileEntry "Personal" "Personal" "brave-personal"
+        // mkProfileEntry "Work" "Work" "brave-work";
     };
 
   };
