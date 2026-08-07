8aa05394f293b413998809e152402fcfe1e0fef6
happymab <happymab.dev@pm.me>
2026-08-07 13:40:12 +0700

    fix: correct device

--- modules/hosts/nixos-vm/disko.nix
+++ modules/hosts/nixos-vm/disko.nix
@@ -1,71 +1,64 @@
 {
   flake.diskoConfigurations.hostNixos-vm = {
     disko.devices = {
-      disk.main = {
-        device = "/dev/sdX";  # Will be replaced with real device during install
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-
-            boot = {
-              name = "boot";
-              size = "1M";
-              type = "EF02";
-            };
-            
-            # EFI/Boot partition
-            esp = {
-              name = "ESP";
-              size = "1G";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
-            };
-
-            # LUKS encrypted root partition
-            root = {
-              name = "root";
-              size = "100%";
-              content = {
-                type = "luks";
-                name = "cryptroot";
-                # disable settings.keyFile if you want to use interactive password entry
-                #passwordFile = "/tmp/secret.key"; # Interactive
-                settings = {
-                  allowDiscards = true;
-                  # keyFile = "/tmp/secret.key";
-                };
-
+      disk = {
+        main = {
+          type = "disk";
+          device = "/dev/vda";
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                size = "512M";
+                type = "EF00";
                 content = {
-                  type = "btrfs";
-                  extraArgs = [ "-f" ];  # Override existing filesystem
-
-                  subvolumes = {
-                    "/root" = {
-                      mountpoint = "/";
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                      ];
-                    };
-                    "/home" = {
-                      mountpoint = "/home";
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                      ];
-                    };
-                    "/nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                      ];
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [ "umask=0077" ];
+                };
+              };
+              luks = {
+                size = "100%";
+                content = {
+                  type = "luks";
+                  name = "crypted";
+                  # disable settings.keyFile if you want to use interactive password entry
+                  #passwordFile = "/tmp/secret.key"; # Interactive
+                  settings = {
+                    allowDiscards = true;
+                    keyFile = "/tmp/secret.key";
+                  };
+                  additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+                  content = {
+                    type = "btrfs";
+                    extraArgs = [ "-f" ];
+                    subvolumes = {
+                      "/root" = {
+                        mountpoint = "/";
+                        mountOptions = [
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                      };
+                      "/home" = {
+                        mountpoint = "/home";
+                        mountOptions = [
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                      };
+                      "/nix" = {
+                        mountpoint = "/nix";
+                        mountOptions = [
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                      };
+                      "/swap" = {
+                        mountpoint = "/.swapvol";
+                        swap.swapfile.size = "20M";
+                      };
                     };
                   };
                 };
