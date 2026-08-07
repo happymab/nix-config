8d988a3d5faf5c167789dffb774646c3c89ab616
happymab <happymab.dev@pm.me>
2026-08-07 14:08:04 +0700

    fix: disable keyfile

--- modules/hosts/nixos-vm/disko.nix
+++ modules/hosts/nixos-vm/disko.nix
@@ -27,7 +27,7 @@
                   #passwordFile = "/tmp/secret.key"; # Interactive
                   settings = {
                     allowDiscards = true;
-                    keyFile = "/tmp/secret.key";
+                    # keyFile = "/tmp/secret.key";
                   };
                   additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
                   content = {
