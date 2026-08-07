71ed7d0950fa4c1cdc01fe64806bb62a9f1bc47f
happymab <happymab.dev@pm.me>
2026-08-07 14:24:16 +0700

    fix: initial password

--- modules/users/mab/home.nix
+++ modules/users/mab/home.nix
@@ -7,7 +7,8 @@
     users.users."mab" = {
       isNormalUser = true;
       shell = pkgs.bash; # define shell
-      initialHashedPassword = "$y$j9T$mqXVyJk/jjF75FmL.6UsV0$N5QecXnSDe94jr9Fxh5NFjMmNSF9a63O5LZb8b9v1l0";
+      # initialHashedPassword = "$y$j9T$mqXVyJk/jjF75FmL.6UsV0$N5QecXnSDe94jr9Fxh5NFjMmNSF9a63O5LZb8b9v1l0";
+      initialPassword = "welcome";
 
       # Add user to groups
       extraGroups = [
@@ -15,7 +16,7 @@
         "wheel"
         "video"
         "render"
-        # "libvirtd"
+        "libvirtd"
         "podman"
       ];
     };
