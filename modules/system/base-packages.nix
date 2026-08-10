d5465b36c43d922e9b386e119ee4e3135d49d0f7
happymab <happymab.dev@pm.me>
2026-08-10 15:22:51 +0000

    research: add shells

--- modules/system/base-packages.nix
+++ modules/system/base-packages.nix
@@ -3,8 +3,17 @@
     # Allow unfree packages (required for some multimedia codecs, NVIDIA drivers, etc.)
     nixpkgs.config.allowUnfree = true;
 
-    # Install firefox.
-    programs.firefox.enable = true;
+    programs = {
+      # Install firefox.
+      firefox.enable = true;
+
+      # Install shells
+      bash.enable = true;
+      zsh.enable = true;
+      fish.enable = true;
+    };
+
+    environment.shells = with pkgs; [ bash zsh fish ];
 
     # List packages installed in system profile
     environment.systemPackages = with pkgs; [
