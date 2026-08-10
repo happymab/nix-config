342e5faf92e4316c6c837e0b18937b7b5befc9f7
happymab <happymab.dev@pm.me>
2026-08-10 07:38:44 +0000

    feat: update locales

--- modules/system/locale.nix
+++ modules/system/locale.nix
@@ -7,23 +7,18 @@
     i18n = {
       defaultLocale = "en_US.UTF-8";
       extraLocales = "all";
-      imperativeLocale = true;
       extraLocaleSettings = {
         LC_ADDRESS = "en_US.UTF-8";
         LC_IDENTIFICATION = "en_US.UTF-8";
-        # LC_MEASUREMENT = "en_US.UTF-8";
-        LC_MEASUREMENT = "en_DK.UTF-8";
+        LC_MEASUREMENT = "en_DK.UTF-8";  # Metric system
         LC_MONETARY = "en_US.UTF-8";
         LC_NAME = "en_US.UTF-8";
         LC_NUMERIC = "en_US.UTF-8";
         LC_PAPER = "en_US.UTF-8";
         LC_TELEPHONE = "en_US.UTF-8";
-        # LC_TIME = "en_US.UTF-8";
-        LC_TIME = "en_DK.UTF-8";
+        LC_TIME = "en_DK.UTF-8";  # 24-hour time format, european date format
       };
     };
-
-    environment.systemPackages = [ pkgs.stdenv.cc.libc.out ];
 
   };
 }
