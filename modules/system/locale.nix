cf122fd7846767a1d27f4a21e961f8440c4cf284
happymab <happymab.dev@pm.me>
2026-08-10 07:16:24 +0000

    feat: update locales

--- modules/system/locale.nix
+++ modules/system/locale.nix
@@ -11,14 +11,14 @@
         LC_ADDRESS = "en_US.UTF-8";
         LC_IDENTIFICATION = "en_US.UTF-8";
         # LC_MEASUREMENT = "en_US.UTF-8";
-        LC_MEASUREMENT = "de_CH.UTF-8";
+        LC_MEASUREMENT = "en_SG.UTF-8";
         LC_MONETARY = "en_US.UTF-8";
         LC_NAME = "en_US.UTF-8";
         LC_NUMERIC = "en_US.UTF-8";
         LC_PAPER = "en_US.UTF-8";
         LC_TELEPHONE = "en_US.UTF-8";
         # LC_TIME = "en_US.UTF-8";
-        LC_TIME = "de_CH.UTF-8";
+        LC_TIME = "en_SG.UTF-8";
       };
     };
   };
