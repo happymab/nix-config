dc26d8d867244d8a929767711a909df22f24679e
happymab <happymab.dev@pm.me>
2026-08-12 06:46:11 +0000

    feat: install fonts

--- modules/system/base-packages.nix
+++ modules/system/base-packages.nix
@@ -11,11 +11,39 @@
     environment.shells = with pkgs; [ bash zsh fish ];
 
     # List packages installed in system profile
-    environment.systemPackages = with pkgs; [
-      git
-      vim
-      wget
-      curl
+    environment.systemPackages = with pkgs; [ git vim wget curl ];
+
+    # Install default fonts
+    fonts.enableDefaultPackages = true;
+
+    # List additional fonts to be installed in system profile
+    fonts.packages = with pkgs; [
+      noto-fonts
+      noto-fonts-color-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      mplus-outline-fonts.githubRelease
+      dina-font
+      proggyfonts
+      ubuntu-sans
+      cm_unicode
+      corefonts
+      unifont
+
+      # Nerd Fonts
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.fira-code
+      nerd-fonts.noto
+      nerd-fonts.geist-mono
+      nerd-fonts.sauce-code-pro
+      nerd-fonts.droid-sans-mono
+      nerd-fonts.iosevka
     ];
+
+    fonts.fontconfig.defaultFonts = {
+      monospace = ["JetBrainsMono Nerd Font"];
+    };
+
   };
 }
