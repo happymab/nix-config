41bd25385d5a620538967e7b339d5028ebd30a51
happymab <happymab.dev@pm.me>
2026-08-19 12:06:24 +0700

    feat: add podman-compose

--- modules/features/podman.nix
+++ modules/features/podman.nix
@@ -7,8 +7,13 @@
         enable = true;
 
         # Enable if containers under podman-compose must be able to talk to each other.
-        defaultNetwork.settings.dns_enabled = false;
+        defaultNetwork.settings.dns_enabled = true;
       };
     };
+
+    # Install podman-compose
+    environment.systemPackages = with pkgs; [
+      podman-compose
+    ];
   };
 }
