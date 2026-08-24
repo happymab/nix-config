8ed468f96bb46418b885fb56929389f4d64315a5
happymab <happymab.dev@pm.me>
2026-08-24 15:33:52 +0700

    feat: add service for daily update of flatpaks

--- modules/system/flatpak.nix
+++ modules/system/flatpak.nix
@@ -3,6 +3,27 @@
     # Enable flatpak support globally
     services.flatpak.enable = true;
 
+    # Define flatpak update service
+    systemd.services.flatpak-update = {
+      description = "Update Flatpak packages";
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+        ExecStart = "${pkgs.flatpak}/bin/flatpak update --non-interactive";
+      };
+    };
+
+    # Define timer to trigger the update service
+    systemd.timers.flatpak-update = {
+      description = "Daily timer for flatpak-update service";
+      timerConfig = {
+        OnCalendar = "daily";
+        RandomizedDelaySec = "30m"; # Up to 30 min random delay to spread load
+        Persistent = true; # Run missed executions after boot
+      };
+      wantedBy = [ "timers.target" ];
+    };
+
     # Add the Flathub repository to the system
     systemd.services.flatpak-repo = {
       wantedBy = [ "multi-user.target" ];
