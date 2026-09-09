ac1e21d516ea426ccc61027d78a7f21683896471
happymab <happymab.dev@pm.me>
2026-09-09 15:14:33 +0700

    feat: migrate to home-manager

--- modules/users/mab/user-mab.nix
+++ modules/users/mab/user-mab.nix
@@ -26,6 +26,14 @@
       ];
     };
 
+    # home-manager wiring for user mab
+    home-manager.useGlobalPkgs = true;
+    home-manager.useUserPackages = true;
+
+    # self/inputs must be forwarded into the home-manager module system,
+    # since your home modules destructure them
+    # home-manager.extraSpecialArgs = { inherit self inputs; };
+
     # enable home-manager for user mab
     home-manager.users.mab = self.homeModules.homeMab;
 
