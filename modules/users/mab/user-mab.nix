adf9fdb6231acc65e04ed8ac18a27fff08e40aee
happymab <happymab.dev@pm.me>
2026-09-09 22:02:12 +0700

    chore: move shell configuration to home-manager

--- modules/users/mab/user-mab.nix
+++ modules/users/mab/user-mab.nix
@@ -12,6 +12,7 @@
     users.users."mab" = {
       isNormalUser = true;
       shell = pkgs.zsh; # define shell
+      ignoreShellProgramCheck = true;
       initialHashedPassword = "$y$j9T$mqXVyJk/jjF75FmL.6UsV0$N5QecXnSDe94jr9Fxh5NFjMmNSF9a63O5LZb8b9v1l0";
 
       # Add user to groups
