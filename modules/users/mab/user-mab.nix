2656498b28abc7e103814183eafeffe1e8052546
happymab <happymab.dev@pm.me>
2026-09-10 18:48:47 +0700

    Revert "feat: install home-manager"
    This reverts commit 5a75ac0868b3288c77100c2f0231f4cb3afd7ff6.

--- modules/users/mab/user-mab.nix
+++ modules/users/mab/user-mab.nix
@@ -26,9 +26,6 @@
       ];
     };
 
-    # Let Home Manager install and manage itself.
-    programs.home-manager.enable = true;
-
     home-manager = {
       useGlobalPkgs = true;
       useUserPackages = true;
