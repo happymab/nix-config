5a75ac0868b3288c77100c2f0231f4cb3afd7ff6
happymab <happymab.dev@pm.me>
2026-09-10 18:41:26 +0700

    feat: install home-manager

--- modules/users/mab/user-mab.nix
+++ modules/users/mab/user-mab.nix
@@ -26,6 +26,9 @@
       ];
     };
 
+    # Let Home Manager install and manage itself.
+    programs.home-manager.enable = true;
+
     home-manager = {
       useGlobalPkgs = true;
       useUserPackages = true;
